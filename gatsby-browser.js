02b183351cd64150d29695b382e12090afc0a06e
John <37978984+johnridesabike@users.noreply.github.com>
2020-10-10 11:15:32 -0400

    Quickfix font css.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 //for importing global styles
 import "normalize.css";
-import "fontsource-cooper-hewitt/all-300.css";
+import "fontsource-cooper-hewitt/all-400.css";
 import "charter-webfont";
 import "@reach/skip-nav/styles.css";
 import "./src/style.css";
