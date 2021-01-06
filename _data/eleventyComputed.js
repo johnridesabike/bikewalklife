404efb6e7efd0692941f28cfaf33cec446e3a64a
John <37978984+johnridesabike@users.noreply.github.com>
2021-01-06 08:29:02 -0500

    Updated eleventyComputed.js

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -6,15 +6,12 @@ const timeZone = "America/New_York";
 module.exports = {
   absoluteUrl: (data) => new URL(data.page.url, config.site_url).href,
   isoDate: (data) => data.page.date.toISOString(),
-  sitemapDate: (data) => {
-    return (
-      data.page.date.toLocaleString(lang, { year: "numeric", timeZone }) +
-      "-" +
-      data.page.date.toLocaleString(lang, { month: "2-digit", timeZone }) +
-      "-" +
-      data.page.date.toLocaleString(lang, { day: "2-digit", timeZone })
-    );
-  },
+  sitemapDate: (data) =>
+    data.page.date.toLocaleString(lang, { year: "numeric", timeZone }) +
+    "-" +
+    data.page.date.toLocaleString(lang, { month: "2-digit", timeZone }) +
+    "-" +
+    data.page.date.toLocaleString(lang, { day: "2-digit", timeZone }),
   formattedDate: (data) =>
     data.page.date.toLocaleString(lang, {
       year: "numeric",
