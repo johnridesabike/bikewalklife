bb16f263a9369859a55786bf2a99bd9d7a095872
John <37978984+johnridesabike@users.noreply.github.com>
2021-01-06 08:28:18 -0500

    Updated eleventyComputed.js

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,44 +1,25 @@
 const config = require("./config.json");
 
-/*
-  For some reason the live preview on Forestry messes up Eleventy's dates?
-*/
-const getDate = (date) => {
-  if (typeof date === "string") {
-    console.warn("DATE WASN'T PARSED CORRECTLY ", date);
-    return new Date(date);
-  } else {
-    return date;
-  }
-};
+const lang = "en-US";
+const timeZone = "America/New_York";
 
 module.exports = {
   absoluteUrl: (data) => new URL(data.page.url, config.site_url).href,
-  isoDate: (data) => getDate(data.page.date).toISOString(),
+  isoDate: (data) => data.page.date.toISOString(),
   sitemapDate: (data) => {
-    const date = getDate(data.page.date);
     return (
-      date.toLocaleString("en-US", {
-        year: "numeric",
-        timeZone: "America/New_York",
-      }) +
+      data.page.date.toLocaleString(lang, { year: "numeric", timeZone }) +
       "-" +
-      date.toLocaleString("en-US", {
-        month: "2-digit",
-        timeZone: "America/New_York",
-      }) +
+      data.page.date.toLocaleString(lang, { month: "2-digit", timeZone }) +
       "-" +
-      date.toLocaleString("en-US", {
-        day: "2-digit",
-        timeZone: "America/New_York",
-      })
+      data.page.date.toLocaleString(lang, { day: "2-digit", timeZone })
     );
   },
   formattedDate: (data) =>
-    getDate(data.page.date).toLocaleString("en-US", {
+    data.page.date.toLocaleString(lang, {
       year: "numeric",
       month: "long",
       day: "numeric",
-      timeZone: "America/New_York",
+      timeZone,
     }),
 };
