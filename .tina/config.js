d1b8580208778ec110d848a660afc2cfe88650d9
John <37978984+johnridesabike@users.noreply.github.com>
2022-12-31 17:20:19 -0500

    Updates

--- .tina/config.js
+++ .tina/config.js
@@ -41,7 +41,6 @@ export default defineConfig({
             label: "Author",
             type: "string",
             options: ["John Jackson"],
-            description: "Enter the author of the post",
             required: true,
           },
           {
@@ -86,6 +85,9 @@ export default defineConfig({
             type: "string",
             required: true,
             description: "This can appear as a preview summary of the post.",
+            ui: {
+              component: "textarea",
+            },
           },
           {
             name: "external_link",
@@ -94,6 +96,16 @@ export default defineConfig({
             required: false,
             description:
               "The URL to the external page this post is about, if applicable.",
+          },
+          {
+            name: "tags",
+            type: "string",
+            list: true,
+            label: "Tags",
+            description: "Tags are used to display posts related to this post.",
+            ui: {
+              defaultItem: () => "untagged",
+            },
           },
           {
             name: "draft",
@@ -126,9 +138,11 @@ export default defineConfig({
           {
             name: "intro",
             label: "Intro",
-            type: "rich-text",
+            type: "string",
             required: true,
-            description: "Enter description here",
+            ui: {
+              component: "textarea",
+            },
           },
           {
             name: "image_large",
@@ -153,7 +167,7 @@ export default defineConfig({
                 required: false,
                 label: "Caption",
                 description:
-                  "This text will appear under the image. It shouldn’t be the same as the <em>alternate text</em>. (This isn't being used.)",
+                  "This text will appear under the image. It shouldn’t be the same as the alternate text. (This isn't being used.)",
               },
             ],
             label: "Image",
@@ -182,7 +196,7 @@ export default defineConfig({
                 required: false,
                 label: "Caption",
                 description:
-                  "This text will appear under the image. It shouldn’t be the same as the <em>alternate text</em>. (This isn't being used.)",
+                  "This text will appear under the image. It shouldn’t be the same as the alternate text. (This isn't being used.)",
               },
             ],
             label: "Image (small)",
@@ -194,6 +208,167 @@ export default defineConfig({
             required: true,
             type: "rich-text",
             isBody: true,
+          },
+        ],
+      },
+      {
+        name: "data",
+        label: "Site data",
+        path: "_data",
+        format: "json",
+        templates: [
+          {
+            name: "ui_strings",
+            label: "UI strings",
+            fields: [
+              {
+                name: "open_linked",
+                label: "Open Linked",
+                type: "string",
+                required: true,
+                description: "Text to display on external linked pages.",
+              },
+              {
+                name: "archive_link",
+                type: "string",
+                required: true,
+                label: "Archive Link",
+                description: "Text to display on the link to the archive.",
+              },
+              {
+                name: "copyright",
+                type: "string",
+                required: true,
+                label: "Footer",
+                description: "Footer content.",
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                name: "contact_title",
+                type: "string",
+                required: true,
+                label: "Contact title",
+                description: "Title above the contact text.",
+              },
+              {
+                name: "contact_text",
+                type: "string",
+                required: true,
+                label: "Contact text",
+                description: "Call-to-action for contact form.",
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                name: "email",
+                type: "string",
+                label: "Email",
+                required: true,
+              },
+              {
+                name: "subscribe_intro",
+                type: "string",
+                required: true,
+                label: "Subscribe intro text",
+                description: "This text introduces the subscription widget.",
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                name: "subscribe_feed_cta",
+                label: "Subscribe call-to-action: feed",
+                type: "string",
+                required: true,
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                name: "subscribe_email_cta",
+                label: "Subscribe call-to-action: email",
+                type: "string",
+                required: true,
+                ui: {
+                  component: "textarea",
+                },
+              },
+            ],
+          },
+          {
+            name: "site_config",
+            label: "Site config",
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+                required: true,
+              },
+              {
+                type: "string",
+                name: "description",
+                label: "Description",
+                required: true,
+              },
+              {
+                type: "string",
+                name: "site_url",
+                label: "Site URL",
+                required: true,
+              },
+              {
+                name: "author_name",
+                type: "string",
+                required: false,
+                label: "Author name",
+                description: "The name of the site’s primary author.",
+              },
+              {
+                name: "lang",
+                type: "string",
+                required: true,
+                options: ["en-US"],
+                label: "Language",
+                description: "The site's language.",
+              },
+              {
+                name: "timeZone",
+                type: "string",
+                required: true,
+                options: ["America/New_York"],
+                label: "Timezone",
+                description: "The timezone used for dates.",
+              },
+              {
+                name: "cloudinary_url",
+                type: "string",
+                required: true,
+                label: "Cloudinary URL",
+              },
+              {
+                name: "twitter_handle",
+                type: "string",
+                required: false,
+                label: "Twitter handle, used for preview cards",
+              },
+            ],
+          },
+          {
+            name: "authors",
+            label: "Authors",
+            fields: [
+              {
+                type: "string",
+                name: "authors",
+                label: "Authors",
+                list: true,
+                required: true,
+              },
+            ],
           },
         ],
       },
