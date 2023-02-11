8e17b133a6e45af60a715cc98e0dae327a6296c9
John <37978984+johnridesabike@users.noreply.github.com>
2023-02-11 16:29:46 -0500

    Init cloudinary media function.

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,43 @@
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "TODO" }),
-  };
-};
+const serverless = require("serverless-http");
+const express = require("express");
+const { Router } = express;
+const { isAuthorized } = require("@tinacms/auth");
+const { createMediaHandler } = require("next-tinacms-cloudinary/dist/handlers");
+
+const app = express();
+
+const router = Router();
+
+const handler = createMediaHandler({
+  authorized: async (req, _res) => {
+    try {
+      if (process.env.NODE_ENV == "development") {
+        return true;
+      }
+
+      const user = await isAuthorized(req);
+
+      return user && user.verified;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  },
+});
+
+router.get("/cloudinary/media", handler);
+
+router.post("/cloudinary/media", handler);
+
+router.delete("/cloudinary/:media", (req, res) => {
+  req.query.media = ["media", req.params.media];
+  return handler(req, res);
+});
+
+app.use("/api/", router);
+
+const serverlessHandler = serverless(app);
+module.exports.handler = async (event, context) => {
+  const result = await serverlessHandler(event, context);
+  return result;
+}
