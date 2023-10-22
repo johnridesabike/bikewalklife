2cf7d70dea6fab45c8ee710b5377fec771a75247
John <37978984+johnridesabike@users.noreply.github.com>
2023-10-22 18:13:00 -0400

    Upgrade Acutis

--- _includes/acutisComponents.js
+++ _includes/acutisComponents.js
@@ -27,138 +27,112 @@ function cloudinaryOptions(opts, image_src) {
   return url.href;
 }
 
-module.exports = [
-  Component.funAsync(
-    "PostCss",
-    Ty.make([["children", Ty.string()]]),
-    ({ children }) => {
-      if (postcssCache.has(children)) {
-        return postcssCache.get(children);
-      } else {
-        const result = postcssWithOptions
-          .process(children, {
-            from: undefined,
-          })
-          .then((result) => result.css);
-        postcssCache.set(children, result);
-        return result;
-      }
-    }
-  ),
+module.exports.PostCss = function PostCss({ children }) {
+  if (postcssCache.has(children)) {
+    return postcssCache.get(children);
+  } else {
+    const result = postcssWithOptions
+      .process(children, {
+        from: undefined,
+      })
+      .then((result) => result.css);
+    postcssCache.set(children, result);
+    return result;
+  }
+};
+module.exports.PostCss.interface = Ty.make([["children", Ty.string()]]);
 
-  Component.funAsync(
-    "Icon",
-    Ty.make([
-      ["name", Ty.string()],
-      ["class", Ty.nullable(Ty.string())],
-    ]),
-    (props) =>
-      Promise.resolve(icons[props.name].toSvg({ class: props.class || "" }))
-  ),
+module.exports.Icon = function Icon(props) {
+  return Promise.resolve(icons[props.name].toSvg({ class: props.class || "" }));
+};
+module.exports.Icon.interface = Ty.make([
+  ["name", Ty.string()],
+  ["class", Ty.nullable(Ty.string())],
+]);
 
-  Component.funAsync("Log", Ty.make([["val", Ty.unknown()]]), (props) => {
-    console.log(props.val);
-    return Promise.resolve("");
-  }),
+module.exports.Log = function Log(props) {
+  console.log(props.val);
+  return Promise.resolve("");
+};
+module.exports.Log.interface = Ty.make([["val", Ty.unknown()]]);
 
-  Component.funAsync(
-    "Link",
-    Ty.make([
-      ["current", Ty.nullable(Ty.string())],
-      ["href", Ty.string()],
-      ["class", Ty.nullable(Ty.string())],
-      ["style", Ty.nullable(Ty.string())],
-      ["tabIndex", Ty.nullable(Ty.int())],
-      ["children", Ty.string()],
-    ]),
-    (props) => {
-      const current =
-        props.current && props.current === props.href
-          ? "aria-current=page"
-          : "";
-      const className = props.class ? `class="${props.class}"` : "";
-      const style = props.style ? `style="${props.style}"` : "";
-      const tabIndex = props.tabIndex ? `tabindex=${props.tabIndex}` : "";
-      return Promise.resolve(
-        `<a href="${props.href}" ${className} ${current} ${style} ${tabIndex}>
-            ${props.children}
-           </a>`
-      );
-    }
-  ),
+module.exports.Link = function Link(props) {
+  const current =
+    props.current && props.current === props.href ? "aria-current=page" : "";
+  const className = props.class ? `class="${props.class}"` : "";
+  const style = props.style ? `style="${props.style}"` : "";
+  const tabIndex = props.tabIndex ? `tabindex=${props.tabIndex}` : "";
+  return Promise.resolve(
+    `<a href="${props.href}" ${className} ${current} ${style} ${tabIndex}>
+      ${props.children}
+    </a>`
+  );
+};
+module.exports.Link.interface = Ty.make([
+  ["current", Ty.nullable(Ty.string())],
+  ["href", Ty.string()],
+  ["class", Ty.nullable(Ty.string())],
+  ["style", Ty.nullable(Ty.string())],
+  ["tabIndex", Ty.nullable(Ty.int())],
+  ["children", Ty.string()],
+]);
 
-  Component.funAsync(
-    "ImgSrc",
-    Ty.make([
-      ["width", Ty.int()],
-      ["aspect", Ty.float()],
-      ["gravity", Ty.string()],
-      ["image", Ty.string()],
-    ]),
-    ({ width, aspect, gravity, image }) => {
-      const height = Math.ceil(width * aspect);
-      const opts = encodeURIComponent(
-        "f_auto," +
-          "q_auto," +
-          "c_fill," +
-          `g_${gravity},` +
-          `h_${height},` +
-          `w_${width}`
-      );
-      try {
-        return Promise.resolve(cloudinaryOptions(opts, image));
-      } catch (e) {
-        console.warn("Invalid image URL:", image);
-        return Promise.resolve(cloudinary_url + opts + image);
-      }
-    }
-  ),
+module.exports.ImgSrc = function ImgSrc({ width, aspect, gravity, image }) {
+  const height = Math.ceil(width * aspect);
+  const opts = encodeURIComponent(
+    `f_auto,q_auto,c_fill,g_${gravity},h_${height},w_${width}`
+  );
+  try {
+    return Promise.resolve(cloudinaryOptions(opts, image));
+  } catch (e) {
+    console.warn("Invalid image URL:", image);
+    return Promise.resolve(cloudinary_url + opts + image);
+  }
+};
+module.exports.ImgSrc.interface = Ty.make([
+  ["width", Ty.int()],
+  ["aspect", Ty.float()],
+  ["gravity", Ty.string()],
+  ["image", Ty.string()],
+]);
 
-  Component.funAsync(
-    "ImgSrcStatic",
-    Ty.make([
-      ["transforms", Ty.list(Ty.string())],
-      ["image", Ty.string()],
-    ]),
-    ({ transforms, image }) => {
-      const opts = transforms.map(encodeURIComponent).join("/");
-      try {
-        return Promise.resolve(cloudinaryOptions(opts, image));
-      } catch (e) {
-        console.warn("Invalid image URL:", image);
-        return Promise.resolve(cloudinary_url + "/" + opts + "/" + image);
-      }
-    }
-  ),
+module.exports.ImgSrcStatic = function ImgSrcStatic({ transforms, image }) {
+  const opts = transforms.map(encodeURIComponent).join("/");
+  try {
+    return Promise.resolve(cloudinaryOptions(opts, image));
+  } catch (e) {
+    console.warn("Invalid image URL:", image);
+    return Promise.resolve(cloudinary_url + "/" + opts + "/" + image);
+  }
+};
+module.exports.ImgSrcStatic.interface = Ty.make([
+  ["transforms", Ty.list(Ty.string())],
+  ["image", Ty.string()],
+]);
 
-  Component.funAsync(
-    "Favicon",
-    Ty.make([
-      ["file", Ty.string()],
-      ["width", Ty.int()],
-    ]),
-    ({ file, width }) => {
-      const key = `${file}-${width}`;
-      if (faviconCache.has(key)) {
-        return faviconCache.get(key);
-      } else {
-        const result = Image(path.join(__dirname, "..", file), {
-          widths: [width],
-          formats: ["png"],
-          urlPath: "/",
-          outputDir: path.join(__dirname, "..", "_site"),
-          filenameFormat: (_id, _src, width, format, _options) =>
-            `favicon-${width}.${format}`,
-        }).then((metadata) => metadata.png[0].url);
-        faviconCache.set(key, result);
-        return result;
-      }
-    }
-  ),
+module.exports.Favicon = function Favicon({ file, width }) {
+  const key = `${file}-${width}`;
+  if (faviconCache.has(key)) {
+    return faviconCache.get(key);
+  } else {
+    const result = Image(path.join(__dirname, "..", file), {
+      widths: [width],
+      formats: ["png"],
+      urlPath: "/",
+      outputDir: path.join(__dirname, "..", "_site"),
+      filenameFormat: (_id, _src, width, format, _options) =>
+        `favicon-${width}.${format}`,
+    }).then((metadata) => metadata.png[0].url);
+    faviconCache.set(key, result);
+    return result;
+  }
+};
+module.exports.Favicon.interface = Ty.make([
+  ["file", Ty.string()],
+  ["width", Ty.int()],
+]);
 
-  Component.funAsync(
-    "PageNumber",
-    Ty.make([["pageNumber", Ty.int()]]),
-    ({ pageNumber }) => Promise.resolve(String(pageNumber + 1))
-  ),
-];
+module.exports.PageNumber = function PageNumber({ pageNumber }) {
+  return Promise.resolve(String(pageNumber + 1));
+};
+module.exports.PageNumber.interface = Ty.make([["pageNumber", Ty.int()]]);
